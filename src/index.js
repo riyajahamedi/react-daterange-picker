653f48bcdd4c0624e77493e2ab53e62839f96446
Riyaj Ahamed <riyajahamedi@hotmail.com>
2022-08-20 14:50:21 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import moment from "moment";
+
+import "./styles.css";
+import DateRangePicker from "react-bootstrap-daterangepicker";
+// you will need the css that comes with bootstrap@3. if you are using
+// a tool like webpack, you can do the following:
+import "bootstrap/dist/css/bootstrap.css";
+// you will also need the css that comes with bootstrap-daterangepicker
+import "bootstrap-daterangepicker/daterangepicker.css";
+
+function App() {
+  const [dates, setDatesState] = useState({
+    startDate: "",
+    endDate: ""
+  });
+
+  const setDates = (e, { startDate, endDate }) => {
+    setDatesState({
+      startDate: startDate.format("YYYY-MM-DD"),
+      endDate: endDate.format("YYYY-MM-DD")
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      {dates.startDate} - {dates.endDate}
+      <br />
+      <DateRangePicker
+        onApply={setDates}
+        ranges={{
+          Today: [moment(), moment()],
+          Yesterday: [
+            moment().subtract(1, "days"),
+            moment().subtract(1, "days")
+          ],
+          "Last 7 Days": [moment().subtract(6, "days"), moment()],
+          "Last 30 Days": [moment().subtract(29, "days"), moment()],
+          "This Month": [moment().startOf("month"), moment().endOf("month")],
+          "Last Month": [
+            moment()
+              .subtract(1, "month")
+              .startOf("month"),
+            moment()
+              .subtract(1, "month")
+              .endOf("month")
+          ]
+        }}
+        startDate="8/1/2019"
+        endDate="8/14/2019"
+      >
+        <input
+          type="text"
+          value={dates.startDate + "-" + dates.endDate}
+          className="form-control"
+        />
+      </DateRangePicker>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
